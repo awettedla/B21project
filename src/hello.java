8e53fac5d9927f137f1f09c06dcf173ddc25292d
awettedla <awettedla@gmail.com>
2020-11-10 12:47:04 -0500

    fix mac and window commit dialog issue

--- src/hello.java
+++ src/hello.java
@@ -1,4 +1,4 @@
 public class hello {
     //more stuff
-    //mac user and windows users are happy to see commit dialog finally
+    //mac user and windows users are happy to see commit dialog finally its there
 }
